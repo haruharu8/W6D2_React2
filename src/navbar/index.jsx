28ce8e242f6d3c91946be7655793b54307ae9d95
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-10-27 15:29:35 -0700

    first commit

---
+++ src/navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavButton from '../navbarbutton'
+
+const Nav = () => {
+  return (
+    <div>
+
+        <NavButton />
+        <NavButton />
+        <NavButton />
+        <NavButton />
+
+    </div>
+  )
+}
+
+export default Nav
